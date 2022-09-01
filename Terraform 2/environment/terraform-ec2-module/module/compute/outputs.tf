616b75add652f447fd98975e6c36cb8df4aa530b
Darcy Wood <104536919+darcyjwood@users.noreply.github.com>
2022-09-01 09:01:28 -0500

    Update outputs.tf

--- Terraform 2/environment/terraform-ec2-module/module/compute/outputs.tf
+++ Terraform 2/environment/terraform-ec2-module/module/compute/outputs.tf
@@ -4,14 +4,3 @@ output "public_ip" {
   value = aws_instance.app_server.public_ip
 }
 
-# output "fingerprint" {
-#   value = data.aws_key_pair.tfec2mod.fingerprint
-# }
-
-# output "name" {
-#   value = data.aws_key_pair.tfec2mod.key_name
-# }
-
-# output "id" {
-#   value = data.aws_key_pair.tfec2mod.id
-# }
